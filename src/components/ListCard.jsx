d19f401c54a8637abce244632903f108751ae3ff
Nahome18 <101459741+Nahome18@users.noreply.github.com>
2025-01-06 16:03:16 -0600

    Home page display fix

---
+++ src/components/ListCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ListCard({num, image, title, artist, album, duration, setCount}){
+    setCount(num+1)
+    return (
+        <div className='list-card'>
+            <span>{num}</span>
+            <img src={image} />
+            <h3>{title}</h3>
+            <h3>{artist}</h3>
+            <h3>{album}</h3>
+            <h3>{duration}</h3>
+        </div>
+    )
+}
