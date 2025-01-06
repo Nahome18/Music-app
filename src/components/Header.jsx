e121f15feb475075fcafc0f86bca0a23c13c5654
Nahome18 <101459741+Nahome18@users.noreply.github.com>
2025-01-06 02:36:46 -0600

    Added Card component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,14 +15,13 @@ function Header({setResults}) {
     const handleSearch = async () => {
 
         try {
-            // const response = await axios.get(`https://api.deezer.com/search?q=${query}`);
-            const response = await axios.get(`/api/search?q=${query}`);
-            setResults(response.data.data);
-            navigate(`/results`); 
-            
+            const response = await fetch(`https://83g2xxt654.execute-api.us-east-1.amazonaws.com/default/search?q=${query}`);
+            const data = await response.json();
+            setResults(data.data);
+            navigate("/results")
         } catch (error) {
             console.error('Error fetching data:', error);
-            alert('Failed to fetch search results');
+            alert('Failed to fetch search result');
         }
     };
 
